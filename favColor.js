9218e57a0dc7f71978b991c35f8d44655e1454e7
Iah M. Jarvis <jiah89358@gmail.com>
2021-04-24 17:00:17 +0000

    stepping away

--- favColor.js
+++ favColor.js
@@ -5,9 +5,9 @@ const favColor = 'Green';
 let randomColors = ['Red', 'Yellow', 'Orange', 'Green', 'Blue', 'Violet']
 // create an array variable of random colors 
 // const variable of favorite color
-/*function myColor(){
+function myFavoriteColor(favColor){
 //create loop that iterates through array for randomColors
-    for(let i = 0; i < randomColors.length; i++){//eureka! green only has 5 chracters so iterate for true using length
+   /*for(let i = 0; i < randomColors.length; i++){//eureka! green only has 5 chracters so iterate for true using length
         if (randomColors.length < 4) {//need to make a condition for character less than 5
             console.log('Is this your color?');
         } else if (randomColors.length > 6) {//condition for characters greater than 6
@@ -17,10 +17,6 @@ let randomColors = ['Red', 'Yellow', 'Orange', 'Green', 'Blue', 'Violet']
         } else {
             console.log(randomColors);
         }
-    } /*curent problem: array element are'nt random, statement wont display
+    } curent problem: array element are'nt random, statement wont display
 }
-myColor();*/
-
-function myFavoriteColor(favColor){
-    for(let i)
-}
+myFavoriteColor(favColor)*/
