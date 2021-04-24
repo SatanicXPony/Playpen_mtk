8dd55492c64e6a26195a3aaf1d66654c6e5ef535
Iah M. Jarvis <jiah89358@gmail.com>
2021-04-24 15:39:47 +0000

    failing miserably

--- favColor.js
+++ favColor.js
@@ -1,22 +1,26 @@
 //start with prompt sync for statement 'Which color?
 
 //set up variables 
-const favColor = "Green";
-let randomColors = ["Red", "Orange", "Yellow", "Blue", "Green", "Indigo", "Violet"];
+const favColor = 'Green';
+let randomColors = ['Red', 'Yellow', 'Orange', 'Green', 'Blue', 'Violet']
 // create an array variable of random colors 
 // const variable of favorite color
-function myColor(favColor){
-//create loop that iterates thru array for randomColors
-    for(let i = 0; i <= 6; i++){//eureka! green only has 5 chracters so iterate for true using length
+/*function myColor(){
+//create loop that iterates through array for randomColors
+    for(let i = 0; i < randomColors.length; i++){//eureka! green only has 5 chracters so iterate for true using length
         if (randomColors.length < 4) {//need to make a condition for character less than 5
             console.log('Is this your color?');
-        } else if (randomColors.length > 5) {//condition for characters greater than 6
+        } else if (randomColors.length > 6) {//condition for characters greater than 6
             console.log('Am I getting warmer?');
-        } else if (randomColors.length === 4) {//condition for only green
+        } else if (randomColors.length === 4 ) {//condition for only green
             console.log('This is your favorite color!')
         } else {
             console.log(randomColors);
         }
-    } //curent problem: instead of randomColors and quote, only the text for line 14 displays
+    } /*curent problem: array element are'nt random, statement wont display
 }
-myColor(favColor);
+myColor();*/
+
+function myFavoriteColor(favColor){
+    for(let i)
+}
