5d684a62079ca826169bedc1ecefb421e0f50328
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-04-28 20:18:37 -0400

    I think i did it...try/catch error

--- hw_playPen/homework_try-catch.js
+++ hw_playPen/homework_try-catch.js
@@ -3,16 +3,19 @@
 
 function divideTwoNumbers(x, y) {
     if (parseFloat(x) !== x) {
-      throw new Error('x is not a number');
+      throw new Error('x is not a number');//put each of these into try block
     }
     if (parseFloat(y) !== y) {
       throw new Error('y is not a number');
     }
     if (y === 0) {
-      throw new Error('divide by zero');
+      throw new Error('divide by zero');//line 12 starts the error
     }
     return x / y;
   }
-  
+  try {
   console.log(divideTwoNumbers(6, 0));
-  
+  } catch(error) {
+    console.log('Congatulations, You Played Yourself');
+  }
+  //question am i changing this log to fit try/catch? or this could be the finally?
