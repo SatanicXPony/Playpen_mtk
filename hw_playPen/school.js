bd1c360b2a4df504d357e4381342a03b721af487
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-05-06 18:12:46 -0400

    nested array for average grades

---
+++ hw_playPen/school.js
@@ -0,0 +1,8 @@
+const schools = [
+    { nane : "high", teachers: [{ name: "Mr. A"}, { name: "Mrs. B"}] },
+    { name: "middle" }, 
+    { name: "elementary" },
+];
+
+
+console.log(schools[0].teachers[1].name);
