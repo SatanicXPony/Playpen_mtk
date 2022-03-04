1e510329c9d5a8adc93bcf1baf2dda984b0f28c0
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-04 00:44:32 -0500

    notes on project

--- hw_playPen/classPlay/neptune.js
+++ hw_playPen/classPlay/neptune.js
@@ -1,15 +1,21 @@
 //create a class for the cpuniverse, the constructs are the characteristics for the cast
 class Cipu {
-  constructor(planet, name, race) {
+  constructor(planet, name, race, charImg) {
+    //add charUrl
     this.planet = planet;
     this.name = name;
     this.race = race;
+    this.charImg = charImg;
   }
   //class specific function or method utilizes class data
   personify() {
     return `${this.name} is an animated impression of the PC-Engine TurboGrafx-16`;
   }
 }
-//the cast is a new instance for cpu i.e the cast are variables that make up the cpuniverse?
+//the cast is a new instance for cpu
 let cast = new Cipu("Planeptune", "Peashy", "Goddess");
 console.log(cast.personify());
+//create main onject for universe then extended classes for the charcters
+
+//goal: need to display characters inside each planet,
+//note: maybe just set planets as buttons instead of modals, just describe planets in headings
