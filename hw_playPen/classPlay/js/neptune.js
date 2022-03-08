1cb9ccee6f98d59fb7db5adac2e90a550f99e4bc
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-08 03:10:10 -0500

    building object infor for parent

--- hw_playPen/classPlay/js/neptune.js
+++ hw_playPen/classPlay/js/neptune.js
@@ -1,2 +1,34 @@
 //start with parent class to be extended by others
 //class typeMoonSeries
+class typeMooniverse {
+  constructor() {
+    this._fateX;
+  }
+}
+class fatexSeries extends typeMooniverse {
+  constructor() {
+    super(); //taking on the methods for typemooniverse instead of rewriting
+  }
+}
+// the object for the series
+this._fateX[
+  ({
+    //are the parens necessary?
+    title: "fate / Extra",
+    setting: "Moon Cell",
+    characters: "Rin Tohsaka", //how to write multiple char to be accessed
+    servants: "Saber",
+    others: "Touko Aouzaki",
+    anime: "true",
+    music: "Ali Project",
+  },
+  {
+    title: "fate/ Extra CCC",
+    setting: "Moon Cell",
+    characters: "Monji Gatou",
+    servants: "Rider",
+    others: "Kirei Kotomine",
+    anime: "false",
+    music: "Kanon Wakashima",
+  })
+];
