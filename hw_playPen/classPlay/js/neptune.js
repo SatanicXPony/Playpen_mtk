03cfa2c3ac4185aaef0a2ee0d8104572c4afb746
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-14 11:01:00 -0400

    made minor edits to div names + method in ln 14

--- hw_playPen/classPlay/js/neptune.js
+++ hw_playPen/classPlay/js/neptune.js
@@ -14,36 +14,34 @@ class typeMooniverse {
 class fatexSeries extends typeMooniverse {
   constructor() {
     super(); //taking on the methods for typemooniverse instead of rewriting
+    this._fatexProfiles = [
+      {
+        title: "fate / Extra",
+        setting: "Moon Cell",
+        masters: "Rin Tohsaka", //maybe seperate masters and spirits into objects within?
+        servants: "Saber", //changer servants to heroic spirits, obj for their true names
+        others: "Touko Aouzaki", //array
+        anime: "true",
+        music: "Ranse Eroica",
+      },
+      {
+        title: "fate/ Extra CCC",
+        setting: "Moon Cell", //how to write the specific variations of worlds on moon cell?
+        masters: "Monji Gatou",
+        servants: "Rider",
+        others: "Kirei Kotomine",
+        anime: "false",
+        music: "Sakura Meikyuu",
+      },
+      {
+        title: "fate/ Last Encore",
+        setting: "Moon Cell",
+        masters: "Misao Amari",
+        servants: "Archer",
+        others: "Fuma Kotaro",
+        anime: "true",
+        music: "Bright Burning Shout",
+      },
+    ];
   }
 }
-// 3. the object for the series to be accessed
-this._fatexProfiles = [
-  {
-    //are the parens necessary?
-    title: "fate / Extra",
-    setting: "Moon Cell",
-    masters: "Rin Tohsaka", //how to write multiple char to be accessed
-    servants: "Saber", //the servants have full names and servant titles idk how to write that out
-    others: "Touko Aouzaki",
-    anime: "true",
-    music: "Ranse Eroica",
-  },
-  {
-    title: "fate/ Extra CCC",
-    setting: "Moon Cell", //how to write the specific variations of worlds on moon cell?
-    masters: "Monji Gatou",
-    servants: "Rider",
-    others: "Kirei Kotomine",
-    anime: "false",
-    music: "Sakura Meikyuu",
-  },
-  {
-    title: "fate/ Last Encore",
-    setting: "Moon Cell",
-    masters: "Misao Amari",
-    servants: "Archer",
-    others: "Fuma Kotaro",
-    anime: "true",
-    music: "Bright Burning Shout",
-  },
-];
