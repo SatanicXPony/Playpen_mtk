1a7dd6cce55fcf99eb4daade21a313b13e3e63fd
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-26 05:49:27 -0400

    note to figure how to display colors

--- hw_playPen/color2.js
+++ hw_playPen/color2.js
@@ -4,7 +4,8 @@ const quote = ["This is not my color.", "This is my favorite color!"];
 function newColor() {
   //Get a Random Color
   let randomColor = Math.floor(Math.random() * colors.length);
-  let content = quote[randomColor]; //
+  //are we doing the full length or the length of a specific color?
+  let content = quote[randomColor]; //may need a conditional to return the value of quote
   console.log(content);
   //   `${Quote}: .text(Quotes[RandomColor]');
   //  $(“#AuthorDisplay”).text(Authors[RandomNumber])`;
