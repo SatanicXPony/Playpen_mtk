1b8f0e84955b179fd236f619a3e034f170130bfa
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-26 05:47:02 -0400

    minor changes to old code

--- hw_playPen/color2.js
+++ hw_playPen/color2.js
@@ -1,11 +1,12 @@
-const Colors = ['Red', 'Yellow', 'Orange', 'Green','Blue', 'Indigo'];
-const Quote = ['This is not my color.', 'This is my favorite color!']
+const colors = ["Red", "Yellow", "Orange", "Green", "Blue", "Indigo"];
+const quote = ["This is not my color.", "This is my favorite color!"];
 
-
-function newColor(){
- //Get a Random Color 
- let RandomColor = Math.floor(Math.random()*Colors.length);
- let content = Quote[RandomColor];//content not read; maybe use an 'onclick' declaration or .log?
- //`${Quote}: .text(Quotes[RandomColor]');
- //$(“#AuthorDisplay”).text(Authors[RandomNumber]);
-};
+function newColor() {
+  //Get a Random Color
+  let randomColor = Math.floor(Math.random() * colors.length);
+  let content = quote[randomColor]; //
+  console.log(content);
+  //   `${Quote}: .text(Quotes[RandomColor]');
+  //  $(“#AuthorDisplay”).text(Authors[RandomNumber])`;
+}
+newColor();
