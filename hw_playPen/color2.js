f06849619aeb8ceb6fa990e83ed5e3402152869b
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-03-26 20:52:29 -0400

    changes to log of colors

--- hw_playPen/color2.js
+++ hw_playPen/color2.js
@@ -4,10 +4,16 @@ const quote = ["This is not my color.", "This is my favorite color!"];
 function newColor() {
   //Get a Random Color
   let randomColor = Math.floor(Math.random() * colors.length);
-  //are we doing the full length or the length of a specific color?
-  let content = quote[randomColor]; //may need a conditional to return the value of quote
-  console.log(content);
-  //   `${Quote}: .text(Quotes[RandomColor]');
-  //  $(“#AuthorDisplay”).text(Authors[RandomNumber])`;
+  if (randomColor === 4) {
+    console.log(colors(quote[1]));
+  } else if (randomColor < 5) {
+    console.log(quote[0]);
+  } else {
+    console.log(colors);
+  }
 }
-newColor();
+newColor(colors);
+
+//console.log(content);
+//   `${Quote}: .text(Quotes[RandomColor]');
+//  $(“#AuthorDisplay”).text(Authors[RandomNumber])`;
