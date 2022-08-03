f1aa81f1e2405b897eeff366cc8cd3986b9e0140
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2022-08-02 20:17:59 -0400

    restart

--- hw_playPen/classPlay/js/wikiData.js
+++ hw_playPen/classPlay/js/wikiData.js
@@ -1,29 +1,23 @@
 //1. start with parent class to be extended by others
-class typeMooniverse {
-  _fatexSettings;
-  //_fatexCharacters
-  //_fatexServants
-  //_others
-  constructor() {} //for each instance return setting and series
-  //2. Methods and properties
-  fatexSettings() {
-    return this._fatexSettings[0].name;
-  }
-}
+// class Typemooniverse {
+//   constructor(fatexSettings) {
+//     this.fatexSettings = fatexSettings;
+//   } //for each instance return setting and series
+//   //2. Methods and properties
+// }
 
-class fatexSeries extends typeMooniverse {
-  constructor() {
-    super(); //taking on the methods for typemooniverse instead of rewriting
-    this._fatexSettings = [
-      {
-        /** everything in here should
-         * call on the series info from fateSer?
-         */
-      },
-    ];
+class FatexSeries {
+  constructor(fatexSettings) {
+    this._fatexSettings = fatexSettings;
   }
 }
-let fatex = new fatexSeries();
+let fatex = new FatexSeries({
+  foo: "bar",
+});
+let fatex2 = new FatexSeries({
+  hello: "world",
+});
+const allOfThem = [fatex, fatex2];
 
 console.log(fatex.fatexSettings());
 
