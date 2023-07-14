7679ee5031e58eb74120a1cf1ea935ced4fe07af
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2023-07-13 21:27:53 -0400

    establishing root resources and attributes

---
+++ jdate_base/erd.js
@@ -0,0 +1,19 @@
+// // In lieu of an ERD, following swapi docs
+
+// //Root: all availilble resources within jdate API
+// {
+//   "books":
+//   "characters":
+//   "locations":
+//   "monsters":
+//   "weapons":
+//   "vehicles":
+// }
+
+// Attributes:
+// books string-- the URL for book resources
+// characters string-- the URL for character resources
+// location string-- the URL for location resources
+// monsters string-- the URL for monster resources
+// weapons string-- the URL for weapon resources
+// vehicles string-- the URL for vehicle resources
